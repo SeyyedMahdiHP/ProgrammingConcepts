373905b229127e1bb13e79402d964420e3fc442a
SeyyedMahdi <SeyyedMahdiHP@gmail.com>
2016-08-11 14:13:53 +0430

    update the repositry and new python conecpts

---
+++ Python C extensions/20. Python C extensions — Python Tips 0.1 documentation_files/readthedocs-dynamic-include.js
@@ -0,0 +1,20 @@
+// RTD Analytics Code
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-17997319-1']);
+_gaq.push(['_trackPageview']);
+
+
+// User Analytics Code
+_gaq.push(['user._setAccount', 'UA-66453801-1']);
+_gaq.push(['user._trackPageview']);
+// End User Analytics Code
+
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+// end RTD Analytics Code
---
+++ map filter lambda( anonymous functions) reduce/4. Map, Filter and Reduce — Python Tips 0.1 documentation_files/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "intermediatepythongithubio",
+    version: "latest",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://book.pythontips.com/en/latest/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/./",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "00d74159"
+  };
+  
